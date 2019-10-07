1dbd0802a280636c6c76bce5f8de1dc53e8c9365
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-10-07 22:55:32 +0000

    Remove animations

--- src/gameObjects/Human.js
+++ src/gameObjects/Human.js
@@ -23,54 +23,12 @@ class Human extends Phaser.GameObjects.Sprite {
 
     console.log(this);
     // create stop animations
-    // this.createStopAnimations();
-    // create walk animations
-    // this.createWalkAnimations();
-  }
-
-  createWalkAnimations() {
-    this.scene.anims.create({
-      key: 'walkFront',
-      frames: this.scene.anims.generateFrameNumbers(this.imageKey, {
-        start: 4,
-        end: 7,
-      }),
-      frameRate: 16,
-      repeat: -1,
-    });
-    this.scene.anims.create({
-      key: 'walkLeft',
-      frames: this.scene.anims.generateFrameNumbers('player', {
-        start: 8,
-        end: 11,
-      }),
-      frameRate: 16,
-      repeat: -1,
-    });
-    // this.anims.create({
-    //   key: 'walkBack',
-    //   frames: this.anims.generateFrameNumbers(this.imageKey, {
-    //     start: 12,
-    //     end: 15,
-    //   }),
-    //   frameRate: 16,
-    //   repeat: -1,
-    // });
-    // this.anims.create({
-    //   key: 'walkRight',
-    //   frames: this.anims.generateFrameNumbers(this.imageKey, {
-    //     start: 16,
-    //     end: 19,
-    //     first: 19,
-    //   }),
-    //   frameRate: 16,
-    //   repeat: -1,
-    // });
+    this.createStopAnimations();
   }
 
   createStopAnimations() {
     this.scene.anims.create({
-      key: 'stopFront',
+      key: `${this.imageKey}Stop`,
       frames: this.scene.anims.generateFrameNumbers(this.imageKey, {
         start: 0,
         end: 0,
@@ -78,45 +36,11 @@ class Human extends Phaser.GameObjects.Sprite {
       frameRate: 16,
       repeat: -1,
     });
-    this.scene.anims.create({
-      key: 'stopLeft',
-      frames: this.scene.anims.generateFrameNumbers(this.imageKey, {
-        start: 1,
-        end: 1,
-      }),
-      frameRate: 16,
-      repeat: -1,
-    });
-    // this.anims.create({
-    //   key: 'stopBack',
-    //   frames: this.anims.generateFrameNumbers(this.imageKey, {
-    //     start: 2,
-    //     end: 2,
-    //   }),
-    //   frameRate: 16,
-    //   repeat: -1,
-    // });
-    // this.anims.create({
-    //   key: 'stopRight',
-    //   frames: this.anims.generateFrameNumbers(this.imageKey, {
-    //     start: 3,
-    //     end: 3,
-    //   }),
-    //   frameRate: 16,
-    //   repeat: -1,
-    // });
   }
 
   stop() {
     // stop animations array
-    const stopAnimations = [
-      'stopFront',
-      'stopLeft',
-      `${this.imageKey}StopBack`,
-      `${this.imageKey}StopRight`,
-    ];
-    // get a random animation number
-    const randomAnimationNumber = Phaser.Math.Between(0, 3);
+    const stopAnimations = [`${this.imageKey}Stop`];
 
     // clear velocity of human
     this.body.setVelocity(0, 0);
@@ -128,29 +52,21 @@ class Human extends Phaser.GameObjects.Sprite {
   moveUp() {
     // set negative velocity in y axis
     this.body.setVelocityY(-this.velocity);
-    // play animation
-    this.play('walkBack');
   }
 
   moveDown() {
     // set velocity in y axis
     this.body.setVelocityY(this.velocity);
-    // play animation
-    this.play('walkFront');
   }
 
   moveLeft() {
     // set negative velocity in x axis
     this.body.setVelocityX(-this.velocity);
-    // play animation
-    // this.play('walkLeft', true);
   }
 
   moveRight() {
     // set velocity in x axis
     this.body.setVelocityX(this.velocity);
-    // play animation
-    this.play(`walkRight`, true);
   }
 
   isClose(human) {
